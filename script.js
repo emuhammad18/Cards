8b15d07be79e0a591414bdac6fb5c50a20fe3b71
ergashev_M <wwwergashevmuxammad18@gmail.com>
2022-12-10 22:35:14 +0500

    Cards

---
+++ script.js
@@ -0,0 +1,20 @@
+function slidesPlugin(activeSlide) {
+  const slides = document.querySelectorAll('.slide');
+
+  slides[activeSlide].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
+}
+slidesPlugin(Math.floor(Math.random()* 6))
